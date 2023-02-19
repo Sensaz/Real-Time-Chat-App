62c058d094e9a7ee53e2bc25103398013204bb0d
Klaudiusz  Wiktor <klaudiusz.wiktor1@gmail.com>
2023-02-19 13:36:36 +0100

    Complete Application

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import { StreamChat } from "stream-chat";
+import { Chat } from "stream-chat-react";
+import Cookies from "universal-cookie";
+
+import {
+  ChannelListContainer,
+  ChanelContainer,
+  Auth,
+} from "./components/index.js";
+
+import "stream-chat-react/dist/css/index.css";
+import "./App.css";
+
+const cookies = new Cookies();
+
+const apiKey = "xrsfvhsqk9g3";
+const authToken = cookies.get("token");
+
+const client = StreamChat.getInstance(apiKey);
+
+if (authToken) {
+  client.connectUser(
+    {
+      id: cookies.get("userId"),
+      name: cookies.get("username"),
+      fullName: cookies.get("fullName"),
+      image: cookies.get("avatarURL"),
+      token: cookies.get("token"),
+      hashedPassword: cookies.get("hashedPassword"),
+      phoneNumber: cookies.get("phoneNumber"),
+    },
+    authToken
+  );
+}
 
 export default function App() {
+  const [createType, setCreateType] = useState("");
+  const [isCreating, setIsCreating] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+
+  if (!authToken) return <Auth />;
+
   return (
-    <div>
-      <h1> Medical Pager Chat App </h1>
+    <div className="app__wrapper">
+      <Chat client={client} theme="team light">
+        <ChannelListContainer
+          isCreating={isCreating}
+          setIsCreating={setIsCreating}
+          setCreateType={setCreateType}
+          setIsEditing={setIsEditing}
+        />
+        <ChanelContainer
+          isCreating={isCreating}
+          setIsCreating={setIsCreating}
+          isEditing={isEditing}
+          setIsEditing={setIsEditing}
+          createType={createType}
+        />
+      </Chat>
     </div>
   );
 }
